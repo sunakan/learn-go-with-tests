56c4d5d0bde49a639b8f5da2c53e2f63507fae29
sunakan <sunakan03@gmail.com>
2019-11-20 20:55:52 +0900

    [Add] 01 Helloworld終了

--- sunabako/hello/hello_test.go
+++ sunabako/hello/hello_test.go
@@ -27,19 +27,31 @@ func TestHello(t *testing.T) {
 	// t.Run("テスト名", 無名関数)でサブテストができるよ
 	// サブサブテストもできるよ
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 		assertCorrectMessage(t, got, want)
 		t.Run("sub sub test", func(t *testing.T) {
-			got := Hello("Suna")
+			got := Hello("Suna", "")
 			want := "Hello, Suna"
 			assertCorrectMessage(t, got, want)
 		})
 	})
 
 	t.Run("empty string defaults to 'World'", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Gabriel", "French")
+		want := "Bonjour, Gabriel"
 		assertCorrectMessage(t, got, want)
 	})
 }
